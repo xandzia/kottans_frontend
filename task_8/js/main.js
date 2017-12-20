c6a8c9cf57140e7f35e1e4042c5a96deb0cf1475
xandzia <stepashko.ann@gmail.com>
2017-12-20 12:31:53 +0200

    task8 MPG: almost done

--- task_8/js/main.js
+++ task_8/js/main.js
@@ -1,6 +1,10 @@
 //images array
 var imagesArray = ['applejack.png', 'boss.png', 'flattershai.png', 'pinkie.png', 'rainbawdash.png', 'rarity.png'];
 
+var flipped = [];
+var numberOfImgs = []; //ids
+var flippedImg = 0;
+
 //copy array, random
 imagesArray = imagesArray.concat(imagesArray);
 
@@ -8,7 +12,6 @@ imagesArray = imagesArray.concat(imagesArray);
 //create 12 cards
 function newGame() {
     flippedImg = 0;
-    var output = '';
     
     imagesArray.sort(function() {
         return 0.5 - Math.random()
@@ -24,33 +27,51 @@ function newGame() {
     //    fig.innerHTML = '<div class="card"><figure class="front"></figure><figure class="back"><img src="img/'+img+'"></figure></div>';
 
         //********test part******
-            fig.innerHTML = '<div class="card" onclick="flip(this, '+i+')"><figure id="'+i+'" class="back"><img src="img/'+imagesArray[i]+'"></figure><figure class="front"><img src="img/front_v5.png"></figure></div>';
+            fig.innerHTML = '<div id="'+i+'" class="card" onclick="flip(this, \''+imagesArray[i]+'\')"><figure class="back"><img src="img/'+imagesArray[i]+'"></figure><figure class="front"><img src="img/front_v5.png"></figure></div>';
         //wrap.appendChild;
 
         //********end test part*****
 
         wrap.appendChild(fig);
-        console.log(i);
     }
     
 }
 
-//animate card
+//action by click
 function flip(card, val) {
+    //animate card
     event.currentTarget.setAttribute('style', 'transform: rotateY(180deg);');
     
-    //var card = event.currentTarget;
-    if (values.length == 0) {
-        //card.style.background = '#ccc';
-        values.push(val);
-        numberOfImgs.push(card.id);
-    }
     
+    if ( flipped.length < 2 ) {
+        if ( flipped.length === 0 ) {
+            //add flipped card to array flipped 
+            flipped.push(val);
+            numberOfImgs.push(card.id);
+        } else if ( flipped.length === 1 ) {
+            flipped.push(val);
+            numberOfImgs.push(card.id);
+            if ( flipped[0] === flipped[1] ) {
+                flippedImg = flipped + 2;
+                //empty arrays
+                flipped = [];
+                numberOfImgs = [];
+                if ( flippedImg === imagesArray.length ) {
+                    alert ('You won');
+                    newGame();
+                }
+            } else {
+                //no match is made
+                    var card1 = document.getElementById(numberOfImgs[0]);
+                    var card2 = document.getElementById(numberOfImgs[1]);
+                    card1.setAttribute('style', 'transform: rotateY(-180deg);');
+                    card2.setAttribute('style', 'transform: rotateY(-180deg);');
+                    //clear arrays
+                    flipped = [];
+                    numberOfImgs = [];
+            }
+        }
+    }  
 }
-
-//hide when the same image
-var values = [];
-var numberOfImgs = []; //ids
-var flippedImg = 0;
 
 newGame();
