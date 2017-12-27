99acea64eeb079db19074449d59551336c69b7fb
xandzia <stepashko.ann@gmail.com>
2017-12-27 21:18:14 +0200

    task_8: add trendy "Arrow functions", "let" "const" instead "var"

--- task_8/js/main.js
+++ task_8/js/main.js
@@ -1,52 +1,44 @@
 //images array
-var imagesArray = ['applejack.png', 'boss.png', 'flattershai.png', 'pinkie.png', 'rainbawdash.png', 'rarity.png'];
+let imagesArray = ['applejack.png', 'boss.png', 'flattershai.png', 'pinkie.png', 'rainbawdash.png', 'rarity.png'];
 
-var flipped = [];
-var numberOfImgs = []; //ids
-var flippedImg = 0;
+let flipped = [];
+let numberOfImgs = []; //ids
+let flippedImg = 0;
 
-var button = document.getElementsByTagName('button')[0];
+const button = document.getElementsByTagName('button')[0];
 
 // Confetti Generator https://www.npmjs.com/package/confetti-js
-var confettiSettings = { target: 'my-canvas' };
-var confetti = new ConfettiGenerator(confettiSettings);
+const confettiSettings = { target: 'my-canvas' };
+let confetti = new ConfettiGenerator(confettiSettings);
 
-//copy array, random
+//copy array
 imagesArray = imagesArray.concat(imagesArray);
 
 
 //create 12 cards
-function newGame() {
+(function newGame() {
     confetti.clear();
     
     button.setAttribute('style', 'display: none;');
     
     flippedImg = 0;
     
-    
     imagesArray.sort(function() {
         return 0.5 - Math.random()
     });
     
-    for(var i = 0; i<12; i++) {
+    for(let i = 0; i<12; i++) {
 
-        var wrap = document.getElementsByClassName('wrapper')[0];
-        var fig = document.createElement('div');
+        const wrap = document.getElementsByClassName('wrapper')[0];
+        const fig = document.createElement('div');
+        
         fig.className = 'container';
-
-
-    //    fig.innerHTML = '<div class="card"><figure class="front"></figure><figure class="back"><img src="img/'+img+'"></figure></div>';
-
-        //********test part******
-            fig.innerHTML = '<div id="'+i+'" class="card" onclick="flip(this, \''+imagesArray[i]+'\')"><figure class="back"><img src="img/'+imagesArray[i]+'"></figure><figure class="front"><img src="img/front_v5.png"></figure></div>';
-        //wrap.appendChild;
-
-        //********end test part*****
+        fig.innerHTML = '<div id="'+i+'" class="card" onclick="flip(this, \''+imagesArray[i]+'\')"><figure class="back"><img src="img/'+imagesArray[i]+'"></figure><figure class="front"><img src="img/front_v5.png"></figure></div>';
 
         wrap.appendChild(fig);
     }
     
-}
+}());
 
 //action by click
 function flip(card, val) {
@@ -61,10 +53,10 @@ function flip(card, val) {
         } else if ( flipped.length === 1 ) {
             flipped.push(val);
             numberOfImgs.push(card.id);
-                    var card1 = document.getElementById(numberOfImgs[0]);
-                    var card2 = document.getElementById(numberOfImgs[1]);
+                    let card1 = document.getElementById(numberOfImgs[0]);
+                    let card2 = document.getElementById(numberOfImgs[1]);
             if ( flipped[0] === flipped[1] ) {
-                setTimeout(function(){
+                setTimeout( () => {
                     card1.setAttribute('style', 'visibility: hidden;');
                     card2.setAttribute('style', 'visibility: hidden;')
                 }, 400);
@@ -73,19 +65,18 @@ function flip(card, val) {
                 flipped = [];
                 numberOfImgs = [];
                 if ( flippedImg === imagesArray.length ) {
-                    setTimeout(function (){
-                       var remove = document.getElementsByClassName('container');
-                        for (var i = remove.length; i--; ) {
+                        confetti.render();
+                    setTimeout( () => {
+                       const remove = document.getElementsByClassName('container');
+                        for (let i = remove.length; i--; ) {
                            remove[i].remove();
                         }
-                        //confirm('You won');
-                        //newGame();
+                        //alert('You won');
                         button.setAttribute('style', 'display: block;');
-                    }, 700);
-                        confetti.render();
+                    }, 1000);
                 }
             } else {
-                function backAgain() {
+                () => {
                     //no match is made
                     card1.setAttribute('style', 'transform: rotateY(0deg);');
                     card2.setAttribute('style', 'transform: rotateY(0deg);');
@@ -97,9 +88,4 @@ function flip(card, val) {
             }
         }
     }  
-};
-
-newGame();
-
-
-
+}
