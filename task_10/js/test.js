a56f355d316760cdc636a5d2dac5852ee0e52b93
xandzia <stepashko.ann@gmail.com>
2018-02-06 01:20:28 +0200

    WA: fetch json by city name

--- task_10/js/test.js
+++ task_10/js/test.js
@@ -1,19 +1,35 @@
-const apiPath = 'https://api.openweathermap.org/data/2.5/weather?q=';
-let userInput = document.getElementById('user-input').value;
+function getWeatherByCityName() {
+    const apiPath = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    const appId = '&APPID=ceae2108078d09147f9148fbef299e0f';
+    const units = '&units=metric';
+    let userInput = document.getElementById('user-input').value;
+    let test = document.getElementById('test');
 
-let city = userInput;
-let url = apiPath+city
-
+    let city = userInput;
+    let url = apiPath+city+units+appId;
+    sendRequest(url);
+    console.log(url);
+    
+};
 function pressEnter(e) {
   if (e.keyCode === 13) {
     e.preventDefault();
-    updateByCity();
+    getWeatherByCityName();
   }
 };
 
 function sendRequest(url){
   fetch(url)
     .then(response => response.json())
+    .then(json => {
+        //const main = {};
+          
+       putIntoHtml(json);
+      console.log(json);
+  })
+};
 
-    })
-}
+
+//function putIntoHtml(weatherInfo) {
+//    test.innerHTML = json;
+//};
