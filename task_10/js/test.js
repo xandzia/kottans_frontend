5df1e05d858bf1c5950394483aa73a3c2c5bcc12
xandzia <stepashko.ann@gmail.com>
2018-02-08 15:57:00 +0200

    WA: add url for 5days, +google autocomplete

--- task_10/js/test.js
+++ task_10/js/test.js
@@ -1,35 +1,65 @@
-function getWeatherByCityName() {
-    const apiPath = 'https://api.openweathermap.org/data/2.5/weather?q=';
-    const appId = '&APPID=ceae2108078d09147f9148fbef299e0f';
-    const units = '&units=metric';
-    let userInput = document.getElementById('user-input').value;
-    let test = document.getElementById('test');
+const userInput = document.getElementById('user-input');
+let latitude = '';
+let longitude = '';
+let cityG = '';
 
-    let city = userInput;
-    let url = apiPath+city+units+appId;
-    sendRequest(url);
-    console.log(url);
-    
+function activatePlacesSearch(){
+    let autocomplete = new google.maps.places.Autocomplete(userInput);   
 };
+
 function pressEnter(e) {
   if (e.keyCode === 13) {
     e.preventDefault();
-    getWeatherByCityName();
+    getCityLatLon();
   }
+};
+
+function getCityLatLon(){
+    let geocoder =  new google.maps.Geocoder();
+    
+    geocoder.geocode( { address: `${userInput.value}`}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            latitude = results[0].geometry.location.lat();
+            longitude = results[0].geometry.location.lng();
+            cityG = results[0].address_components[0].long_name;
+            
+            getWeather();
+        } else {
+            console.log("Something got wrong " + status);
+        }
+    });
+};
+
+function getWeather() {
+    const apiPath = 'https://api.openweathermap.org/data/2.5/forecast?lat=';
+    const appId = '&APPID=ceae2108078d09147f9148fbef299e0f';
+    
+    let url = `${apiPath}${latitude}&lon=${longitude}${appId}`;
+    sendRequest(url);    
 };
 
 function sendRequest(url){
   fetch(url)
     .then(response => response.json())
     .then(json => {
-        //const main = {};
+      const main = {};
+      main.city = json.city.name;
+      main.day = json.list[0].dt_txt;
+      main.temp = json.list[0].main.temp;
+      main.description = json.list[0].weather[0].description;
+      main.icon = json.list[0].weather[0].icon;
           
-       putIntoHtml(json);
+      putIntoHtml(main);
       console.log(json);
+      console.log(main);
   })
 };
 
+function putIntoHtml(main) {
+    const city = document.getElementById('city');
+    const cityGoo = document.getElementById('cityG');
 
-//function putIntoHtml(weatherInfo) {
-//    test.innerHTML = json;
-//};
+    city.innerHTML = main.city;
+    cityGoo.innerHTML = cityG;
+};
