250eb87326b419d08bc1dc53189698d33c6fef46
xandzia <stepashko.ann@gmail.com>
2018-02-15 05:38:59 +0200

    WA: add next 4days in html, CSS: edit buttons

--- task_10/js/test.js
+++ task_10/js/test.js
@@ -1,13 +1,7 @@
-//selectors
 const userInput = document.getElementById('user-input');
 const oneDay = document.querySelector('.one-day');
+const days = document.querySelector('.enother-days');
 const nyanCat = document.querySelector('.wrapper-cat');
-//const city = document.getElementById('city');
-//      day = document.querySelector('.day'),
-//      temperature = document.querySelector('.temp'),
-//      humidity =  document.querySelector('.humid'),
-//      icon = document.querySelector('.icon');
-
 
 let latitude = '';
 let longitude = '';
@@ -22,6 +16,7 @@ function pressEnter(e) {
     getCityLatLon();
     clearHtml(oneDay);
     clearHtml(nyanCat);
+    clearStyle(nyanCat, "style", "-webkit-animation: animateC 4s linear; animation-fill-mode: forwards;");
   }
 };
 
@@ -30,32 +25,37 @@ function getCityLatLon(){
     
     geocoder.geocode( { address: `${userInput.value}`}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results);
+//            console.log(results);
             latitude = results[0].geometry.location.lat();
             longitude = results[0].geometry.location.lng();
             
-            getWeather();
+            getUrl();
         } else {
             console.log("Something got wrong " + status);
-            nyanCat.innerHTML = `<div class='rainbow'>
-                                    <span></span>
-                                </div>
-                                <div class='nyan-cat'>
-                                    <span class="error">not found 404</span>
-                                    <div class='feet'></div>
-                                    <div class='tail'>
-                                        <span></span>
-                                    </div>
-                                    <div class='body'></div>
-                                    <div class='head'></div>
-                                </div>`;
-            nyanCat.setAttribute("style", "-webkit-animation: animateC 4s linear; animation-fill-mode: forwards;");
+            
+            placeNyanCat(nyanCat);
         }
     });
 };
 
-function getWeather() {
-    const apiPath = 'https://api.weatherbit.io/v2.0/forecast/3hourly';
+function placeNyanCat(elem) {
+    elem.innerHTML = `<div class='rainbow'>
+        <span></span>
+        </div>
+        <div class='nyan-cat'>
+        <span class="error">not found 404</span>
+        <div class='feet'></div>
+        <div class='tail'>
+        <span></span>
+        </div>
+        <div class='body'></div>
+        <div class='head'></div>
+        </div>`;
+    elem.setAttribute("style", "-webkit-animation: animateC 4s linear; animation-fill-mode: forwards;");  
+};
+
+function getUrl() {
+    const apiPath = 'https://api.weatherbit.io/v2.0/forecast/daily';
     const appKey = '&key=ca6bb30119264fe2b2608a31b5c79d8e';
     
     let url = `${apiPath}?lat=${latitude}&lon=${longitude}${appKey}`;
@@ -66,6 +66,12 @@ function sendRequest(url){
   fetch(url)
     .then(response => response.json())
     .then(json => {
+      createMainJson(json);
+      console.log(json);
+  })
+};
+
+function createMainJson(json) {
       const main = {};
       main.city = json.city_name;
       main.day = json.data[0].datetime;
@@ -73,29 +79,119 @@ function sendRequest(url){
       main.tempFeel = json.data[0].app_temp;
       main.description = json.data[0].weather.description;
       main.humidity = json.data[0].rh;
-      main.icon = json.data[0].weather.icon;
-          
-      putIntoHtml(main);
-      console.log(json);
+      main.icon = json.data[0].weather.code;
+    
       console.log(main);
-  })
+      putHtmlToday(main);
+      putHtmlDays(json);
 };
 
-function putIntoHtml(main) {
-    oneDay.innerHTML = `<h2 class="city">${main.city}</h2>
+function putHtmlToday(main) {
+    oneDay.innerHTML = `<div class="btn-group">
+                            <button class="f">F</button>
+                            <button class="c">C</button>
+                        </div>
+                        <h2 class="city">${main.city}</h2>
                         <div class="centre">
-                            <span class="temp">${main.temp}<sup>o</sup></span>
+                            <span class="temp">${main.temp}<sup>o</sup>C</span>
                             <img class="icon" src="img/icons/big/cloud.png" alt="">
                         </div>
                         <p class="discriptions">
-                            <time class="d-time" datatime="${main.day}">${main.day}</time>                
-                            <span class="humid">${main.description} ${main.humidity}%</span>
+                            <time class="d-time" datatime="${main.day}">${changeDateTime(main.day)}</time>   
+                            <span class="humid">${main.description}  ${main.humidity}%</span>
                         </p>`;
+};
+
+function putHtmlDays(json) {
+    for (let i = 1; i<5; i++) {
+        let nextDay = document.createElement('div');
+        let datetime = json.data[i].datetime;
+        let rename = changeDateTime(datetime);
+        console.log(rename);
+        nextDay.className = 'a-day';
+        nextDay.innerHTML = `<time datatime="${datetime}">${rename}</time>
+                             <img src="img/icons/big/cloud.png" alt="" class="icon">
+                             <span class="temp-">${json.data[i].temp}&deg;C</span>`;
+        days.appendChild(nextDay);
+    }
+};
+
+function changeDateTime(date) {
+    date = date.split('-');
+    let mon = date[1];
+    switch(mon) {
+        case '01': mon = 'Jan ';
+            break;
+        case '02': mon = 'Feb ';
+            break;
+        case '03': mon = 'Mar ';
+            break;
+        case '04': mon = 'Apr ';
+            break;
+        case '05': mon = 'May ';
+            break;
+        case '06': mon = 'Jun ';
+            break;
+        case '07': mon = 'Jul ';
+            break;
+        case '08': mon = 'Aug ';
+            break;
+        case '09': mon = 'Sept ';
+            break;
+        case '10': mon = 'Oct ';
+            break;
+        case '11': mon = 'Nov ';
+            break;
+        case '12': mon = 'Dec ';
+            break;
+    }
+    date = mon+date[2];
+    return date;
 };
 
 function clearHtml(elem) {
     while (elem.firstChild) {
         elem.removeChild(elem.firstChild);
     }
-    elem.removeAttribute("style", "-webkit-animation: animateC 4s linear; animation-fill-mode: forwards;");
 };
+
+function clearStyle(elem, style) {
+    elem.removeAttribute(style);    
+};
+
+function getIcon(iconCode) {
+    const icons = {
+        cloud: 'cloud',
+        cloudy: 'cloudy',
+        doubleCloudy: 'double-cloudy',
+        doubleRain: 'double-rain',
+        rain: 'rain',
+        rainbow: 'rainbow',
+        snowflake: 'snowflake',
+        snowy: 'snowy',
+        storm: 'storm',
+        stormRain: 'storm-rain',
+        sun: 'sun'
+    };
+    let icon='';
+    switch(iconCode) {
+        case ('200'||'201'||'202'): icon = stormRain;
+            break;
+        default: icon = sun;
+            break;
+    }
+    return `<img class="icon" src="img/icons/big/${icon}.png" alt="${icon}">`;
+};
+
+
+
+//const get = url => fetch(url).then(res => {
+//    if (res.ok) {
+//        return res.json()
+//    }
+//    throw new Error();
+//})
+//const renderUser = userData => {
+//    const li = document.createElement('li');
+//    li.
+//}
